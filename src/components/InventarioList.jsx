1a379180f074a91fdcc92ef6f632be0a754288cb
MLGM2001 <canonmauricio63@gmail.com>
2024-10-01 01:41:00 -0400

    modificacion del inventario

--- src/components/InventarioList.jsx
+++ src/components/InventarioList.jsx
@@ -59,8 +59,81 @@
 // export default InventarioList;
 
 
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import '../components/Styles/InventarioList.css'; // Asegúrate de tener este archivo de estilos
+
+// const InventarioList = () => {
+//   const [inventario, setInventario] = useState([]);
+
+//   useEffect(() => {
+//     const fetchInventario = async () => {
+//       try {
+//         const token = localStorage.getItem('token');
+//         const response = await axios.get('https://backedn-nuevo.onrender.com/api/inventario/detalles', {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+//         setInventario(response.data);
+//       } catch (error) {
+//         console.error('Error al obtener el inventario', error);
+//       }
+//     };
+//     fetchInventario();
+//   }, []);
+
+//   return (
+//     <div className="inventario-container">
+//       <h2 className="titulo-inventario">Inventario de Productos</h2>
+//       <table className="inventario-tabla">
+//         <thead>
+//           <tr>
+//             <th>ID</th>
+//             <th>Nombre del Producto</th>
+//             <th>Descripción</th>
+//             <th>Precio</th>
+//             <th>Color</th>
+//             <th>Cantidad Disponible</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {inventario.map((item, index) => (
+//             <tr key={index}>
+//               <td>{item.id}</td>
+//               <td>{item.nombreProducto}</td>
+//               <td>{item.descripcionProducto}</td>
+//               <td>${item.precioProducto}</td>
+//               <td>
+//                 <div
+//                   className="color-cuadro"
+//                   style={{
+//                     backgroundColor: item.codigoHexColor,
+//                   }}
+//                 ></div>
+//                 <span style={{ marginLeft: '10px' }}>{item.nombreColor}</span>
+//               </td>
+//               <td>{item.cantidad}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default InventarioList;
+
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 import '../components/Styles/InventarioList.css'; // Asegúrate de tener este archivo de estilos
 
 const InventarioList = () => {
@@ -70,7 +143,7 @@ const InventarioList = () => {
     const fetchInventario = async () => {
       try {
         const token = localStorage.getItem('token');
-        const response = await axios.get('https://backedn-nuevo.onrender.com/api/inventario/detalles', {
+        const response = await axios.get('http://localhost:8080/api/inventario/detalles', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -82,6 +155,38 @@ const InventarioList = () => {
     };
     fetchInventario();
   }, []);
+
+  const exportPDF = () => {
+    const doc = new jsPDF();
+    doc.setFontSize(18);
+    doc.text('Inventario de Productos', 14, 22);
+
+    const columns = [
+      { header: 'ID', dataKey: 'id' },
+      { header: 'Nombre del Producto', dataKey: 'nombreProducto' },
+      { header: 'Descripción', dataKey: 'descripcionProducto' },
+      { header: 'Precio', dataKey: 'precioProducto' },
+      { header: 'Color', dataKey: 'nombreColor' },
+      { header: 'Cantidad Disponible', dataKey: 'cantidad' }
+    ];
+
+    const rows = inventario.map((item) => ({
+      id: item.id,
+      nombreProducto: item.nombreProducto,
+      descripcionProducto: item.descripcionProducto,
+      precioProducto: `$${item.precioProducto}`,
+      nombreColor: item.nombreColor,
+      cantidad: item.cantidad,
+    }));
+
+    doc.autoTable({
+      columns: columns,
+      body: rows,
+      startY: 30,
+    });
+
+    doc.save('inventario_productos.pdf');
+  };
 
   return (
     <div className="inventario-container">
@@ -118,6 +223,10 @@ const InventarioList = () => {
           ))}
         </tbody>
       </table>
+      {/* Botón de descarga colocado debajo de la tabla */}
+      <div className="button-container">
+        <button onClick={exportPDF} className="export-button">Descargar PDF</button>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,6 +67,109 @@
 
 
 
+// import React, { useState, useEffect } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import '../components/Styles/Navbar.css';
+
+// const Navbar = ({ onLogout }) => {
+//   const navigate = useNavigate();
+//   const [roles, setRoles] = useState([]);
+//   const [menuOpen, setMenuOpen] = useState(false); // Estado para controlar el menú hamburguesa
+//   const [dropdownOpen, setDropdownOpen] = useState({}); // Estado para manejar submenús
+
+//   const handleLogout = (e) => {
+//     e.preventDefault();
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('roles');
+//     onLogout();
+//     navigate('/login');
+//   };
+
+//   const handleMenuToggle = () => {
+//     setMenuOpen(!menuOpen);
+//   };
+
+//   const toggleDropdown = (menu) => {
+//     setDropdownOpen((prevState) => ({
+//       ...prevState,
+//       [menu]: !prevState[menu],
+//     }));
+//   };
+
+//   useEffect(() => {
+//     const storedRoles = JSON.parse(localStorage.getItem('roles'));
+//     if (storedRoles) {
+//       setRoles(storedRoles);
+//     }
+//   }, []);
+
+//   return (
+//     <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
+//       <h2 className="navbar-logo">Style Store</h2>
+//       {/* Botón de menú hamburguesa */}
+//       <button className="menu-toggle" onClick={handleMenuToggle}>
+//         &#9776; {/* Símbolo de menú hamburguesa */}
+//       </button>
+//       <ul className="navbar-links">
+//         <li><Link to="/">Inicio</Link></li>
+//         <li><Link to="/perfil">Perfil</Link></li>
+//         <li><Link to="/carritodecompras">Carrito</Link></li>
+
+//         {/* Menú de Catálogo */}
+//         <li
+//           className={`productos-dropdown ${dropdownOpen['catalogo'] ? 'active' : ''}`}
+//           onClick={() => toggleDropdown('catalogo')}
+//         >
+//           <Link to="#">Catálogo</Link>
+//           <ul className="dropdown-menu">
+//             <li><Link to="/productolist">Ver Catálogo de Prendas</Link></li>
+//             {(roles.includes('ROLE_ADMIN') || roles.includes('ROLE_ADMIN_DE_PRODUCTOS')) && (
+//               <li><Link to="/crear-producto">Crear Prenda</Link></li>
+//             )}
+//           </ul>
+//         </li>
+
+//         {/* Menú de Categorías */}
+//         <li
+//           className={`productos-dropdown ${dropdownOpen['categorias'] ? 'active' : ''}`}
+//           onClick={() => toggleDropdown('categorias')}
+//         >
+//           <Link to="#">Categorías</Link>
+//           <ul className="dropdown-menu">
+//             {/* Mostrar solo la vista de 'Productos por Categoría' si el rol es 'ROLE_USER' */}
+//             {roles.includes('ROLE_USER') && (
+//               <li><Link to="/productosporcategoria">Productos por Categoría</Link></li>
+//             )}
+//             {/* Mostrar todas las opciones si el rol es 'ROLE_ADMIN' o 'ROLE_ADMIN_DE_PRODUCTOS' */}
+//             {(roles.includes('ROLE_ADMIN') || roles.includes('ROLE_ADMIN_DE_PRODUCTOS')) && (
+//               <>
+//                 <li><Link to="/crearcategoria">Crear Categoría</Link></li>
+//                 <li><Link to="/asignarproductoscategoria">Asignar Productos a Categoría</Link></li>
+//                 <li><Link to="/productosporcategoria">Productos por Categoría</Link></li>
+//               </>
+//             )}
+//           </ul>
+//         </li>
+
+
+//         {roles.includes('ROLE_ADMIN') && (
+//           <>
+//             <li><Link to="/usuarios">Usuarios</Link></li>
+            
+//           </>
+//         )}
+
+
+        
+//         <li><Link to="/logout" onClick={handleLogout}>Cerrar Sesión</Link></li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../components/Styles/Navbar.css';
@@ -151,16 +254,24 @@ const Navbar = ({ onLogout }) => {
           </ul>
         </li>
 
-
-        {roles.includes('ROLE_ADMIN') && (
-          <>
-            <li><Link to="/usuarios">Usuarios</Link></li>
-            
-          </>
+        {/* Menú Gestión solo para ADMIN y ADMIN_DE_PRODUCTOS */}
+        {(roles.includes('ROLE_ADMIN') || roles.includes('ROLE_ADMIN_DE_PRODUCTOS')) && (
+          <li
+            className={`productos-dropdown ${dropdownOpen['gestion'] ? 'active' : ''}`}
+            onClick={() => toggleDropdown('gestion')}
+          >
+            <Link to="#">Gestión</Link>
+            <ul className="dropdown-menu">
+              {/* Mostrar la opción de Usuarios solo para ADMIN */}
+              {roles.includes('ROLE_ADMIN') && (
+                <li><Link to="/usuarios">Usuarios</Link></li>
+              )}
+              {/* Mostrar la opción de Inventario para ADMIN y ADMIN_DE_PRODUCTOS */}
+              <li><Link to="/inventariolist">Inventario</Link></li>
+            </ul>
+          </li>
         )}
 
-
-        
         <li><Link to="/logout" onClick={handleLogout}>Cerrar Sesión</Link></li>
       </ul>
     </nav>
